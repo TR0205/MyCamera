289eba19e8f45fea8fd1574155392cc4dc28cefd
TR0205 <index0205@gmail.com>
2023-02-12 10:49:37 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 對馬篤 on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
